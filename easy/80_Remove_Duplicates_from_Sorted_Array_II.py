543f894aada21524d4e094b4f12ac1696568e537
Irfan Thomson <irfanthomson@gmail.com>
2023-05-06 11:19:41 -0500

    problem 80

---
+++ easy/80_Remove_Duplicates_from_Sorted_Array_II.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i,k = 0,1
+        for n in nums:
+            if nums[i] != n:
+                i += 1
+                nums[i] = n
+                k += 1
+        return k
