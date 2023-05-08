a5d8c236e963f13b115a84c92a008982a5826f1e
Irfan Thomson <irfanthomson@gmail.com>
2023-05-08 09:15:49 -0500

    problem 169

---
+++ easy/169_Majority_Element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = -1
+        for n in nums:
+            if count == 0:
+                candidate = n
+                count = 1
+            else:
+                count = count + 1 if candidate == n else count - 1
+        return candidate
