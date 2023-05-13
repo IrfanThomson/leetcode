799eda8177e0ad67ef8c74eebb9b5a9909b487e4
Irfan Thomson <irfanthomson@gmail.com>
2023-05-13 08:29:26 -0500

    problem 45

---
+++ medium/45_Jump_Game_II.py
@@ -0,0 +1,11 @@
+class Solution:
+    # O(n), dynamic programming
+    def jump(self, nums: List[int]) -> int:
+        nums[len(nums)-1] = 0
+        for i in range(len(nums)-2, -1, -1):
+            min_jump = 10001
+            for j in range(1, nums[i]+1):
+                if i+j < len(nums):
+                    min_jump = nums[i+j]+1 if nums[i+j]+1 < min_jump else min_jump
+            nums[i] = min_jump
+        return nums[0]
